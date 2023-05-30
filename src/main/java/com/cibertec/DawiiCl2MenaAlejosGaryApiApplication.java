bf31febc55a6dc014c4b28093190ce5ef060d2a7
GaryFrZero <garyesp@hotmail.com>
2023-05-30 02:08:11 -0500

    Inicio Proyecto API

---
+++ src/main/java/com/cibertec/DawiiCl2MenaAlejosGaryApiApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl2MenaAlejosGaryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl2MenaAlejosGaryApiApplication.class, args);
+	}
+
+}
